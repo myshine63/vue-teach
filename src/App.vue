ab10c29b084696a985b32b97791605ead5330322
Pan HE <hepan@bluepulse-inc.com>
2025-04-03 18:23:00 +0800

    feat: 创建一个vue,完成项目初始化

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    header
+  </div>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+  vue 教学
+</div>
+</template>
+
+<style scoped>
+
+</style>
